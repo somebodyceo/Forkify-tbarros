fcec09949589344a28ef311a03ac2b56be16539a
somebodyceo <somebodyceo@gmail.com>
2021-01-01 23:57:07 +0100

    Added API key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '436c3db4-c45e-4248-8472-a1d4e67977e2';
 export const MODAL_CLOSE_SEC = 2.5;
